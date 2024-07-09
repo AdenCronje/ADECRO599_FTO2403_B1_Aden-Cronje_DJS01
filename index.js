bb96d8c5ebbde3506d0907f4c01ea8ef92eb0e74
Aden Cronje <adencronje05@gmail.com>
2024-07-09 11:56:51 +0200

    Added parameters into an object

--- index.js
+++ index.js
@@ -6,6 +6,16 @@
  */
 
 // Given Parameters
+// const knownParameters = {
+//   vel: 10000,
+//   acc: 3,
+//   time: 3600,
+//   d: 0,
+//   fuel: 5000,
+//   fbr: 0.5,
+// };
+// console.log(knownParameters);
+
 const vel = 10000; // velocity (km/h)
 const acc = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
@@ -13,22 +23,15 @@ const d = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
-
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const d2 = (d + vel) * time; //calcultes new distance
+const rf = fbr * time; //calculates remaining fuel
+const vel2 = calcNewVel(acc, vel, time); //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
-}
+calcNewVel = (vel, acc, time) => {
+  return vel + acc * time;
+};
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
 console.log(`Corrected Remaining Fuel: ${rf} kg`);
-
-
-
-
-
-
