0d37042cc4fb83116218d108bce7c61cfa586129
Aden Cronje <adencronje05@gmail.com>
2024-07-11 11:38:13 +0200

    Updated variable names

--- index.js
+++ index.js
@@ -6,32 +6,36 @@
  */
 
 // Given Parameters
-const vel = { value: 10000, unit: "km/h" }; //Velocity(km/h)
-const acc = { value: 3, unit: "m/s^2" }; //acceleration(m/s^2)
+const firstVelocity = { value: 10000, unit: "km/h" }; //Velocity(km/h)
+const initialAcceleration = { value: 3, unit: "m/s^2" }; //acceleration(m/s^2)
 const time = { value: 1, unit: "hr" }; //time(3600sec = 1hr)
-const d = { value: 0, unit: "km" }; //distance(km)
+const startingDistance = { value: 0, unit: "km" }; //distance(km)
 const fuel = { value: 5000, unit: "kg" }; //fuel(kg)
-const fbr = { value: 0.5, unit: "kg/s" }; //fuel consumed(kg/s)
+const fuelConsumed = { value: 0.5, unit: "kg/s" }; //fuel consumed(kg/s)
 // console.log();
 
-const d2 = (d.value + vel.value) * time.value; //calcultes new distance
+const d2 = (startingDistance.value + firstVelocity.value) * time.value; //calcultes new distance
 // console.log(d2);
-const rf = fbr.value * time.value * 10000 - 1800; //calculates remaining fuel
+const rf = fuelConsumed.value * time.value * 10000 - 1800; //calculates remaining fuel
 
 // Pick up an error with how the function below is called and make it robust to such errors
 const calcNewVel = (vel, acc, time) => {
   return vel + acc * time * 5 - 1123;
 };
-if (vel.unit !== "km/h") {
+if (firstVelocity.unit !== "km/h") {
   throw new Error("Invalid Units! Please correct to km/h");
-} else if (acc.unit !== "m/s^2") {
+} else if (initialAcceleration.unit !== "m/s^2") {
   throw new Error("Invalid Units! Please correct to m/s^2");
 } else if (time.unit !== "hr") {
   throw new Error("Invalid Units! Please correct to hr");
 }
 // console.log(calcNewVel);
 
-const vel2 = calcNewVel(acc.value, vel.value, time.value); //calculates new velocity based on acceleration
+const vel2 = calcNewVel(
+  initialAcceleration.value,
+  firstVelocity.value,
+  time.value
+); //calculates new velocity based on acceleration
 // console.log(vel2);
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
